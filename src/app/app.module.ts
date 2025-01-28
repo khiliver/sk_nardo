db549fc192f01785950254b898decc69536a4319
Leenard <07213176@dwc-legazpi.edu>
2025-01-28 17:25:23 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/post-create/post.create.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post/post-create/post.create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'post-create',
+    templateUrl: '/post.create.component.html',
+    styleUrls: ['./post.create.component.css'],
+})
+
+export class PostCreateComponent{
+    NewPost = 'This is the initial post';
+    EnteredValue = '';
+        onAddPost(){
+            this.NewPost = this.EnteredValue;
+        };
+}
